b39db84aa4c74401b01e4a58f6ead73e8fd0bca7
vazkir <vazz_m@hotmail.com>
2022-05-18 17:39:31 +0200

    Initial Commit

---
+++ PyTorchDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PyTorchDemo
+//
+//  Created by Vasco Meerman on 18/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
